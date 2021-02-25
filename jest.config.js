f707b01a7c8d048e154898bedc2dfc1f58338cd3
Zhihao Shen <zhihaoshen7@qq.com>
2021-02-25 14:03:15 +0800

    🐞 fix: add code coverage report for codecov.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{js,jsx}', '!**/node_modules/**', '!**/vendor/**'],
+  coverageDirectory: './coverage'
 };
