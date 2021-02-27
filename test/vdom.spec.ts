ece1e096d4077d37e6cb1b1bac481dec0a469a58
Zhihao Shen <zhihaoshen7@qq.com>
2021-02-27 19:51:42 +0800

    🛡 test(core): support unit test in DOM manipulation.

--- test/vdom.spec.ts
+++ test/vdom.spec.ts
@@ -1,18 +1,43 @@
 import El from '../src/vdom';
 
-test("test El'sconstructor", () => {
-  let el = new El('ul', {class: 'list'}, [
-    new El('li', {id: 1}, 'apple'),
-    new El('li', {id: 2}, 'banana'),
-    new El('li', {id: 3}, 'tomato')
-  ]);
+// JQuery-like selector
+function $(selector: string): HTMLElement | null {
+  return document.querySelector(selector);
+}
 
-  expect(el.tagName).toEqual('ul');
-  expect(el.attribute).toEqual({class: 'list'});
+describe("test El's constructor", () => {
+  test("test El's normal constructor", () => {
+    const el = new El('ul', {class: 'list'}, [
+      new El('li', {id: 1}, 'apple'),
+      new El('li', {id: 2}, 'banana'),
+      new El('li', {id: 3}, 'tomato')
+    ]);
 
-  const children = el.children as El[];
-  expect(children).toHaveLength(3);
-  expect(children[0].tagName).toEqual('li');
-  expect(children[0].attribute).toEqual({id: 1});
-  expect(children[0].children).toEqual('apple');
+    expect(el.tagName).toEqual('ul');
+    expect(el.attribute).toEqual({class: 'list'});
+
+    const children = el.children as El[];
+    expect(children).toHaveLength(3);
+    expect(children[0].tagName).toEqual('li');
+    expect(children[0].attribute).toEqual({id: 1});
+    expect(children[0].children).toEqual('apple');
+  });
+});
+
+describe("test El's method", () => {
+  test("test El's render method", () => {
+    const root = new El('ul', {class: 'list'}, [
+      new El('li'),
+      new El('li', null, 'banana'),
+      new El('li', {id: 3}, 'tomato')
+    ]);
+
+    document.body.appendChild(root.render());
+
+    expect($('ul')?.getAttribute('class')).toEqual('list');
+    expect($('ul')?.children.length).toEqual(3);
+
+    expect($('li:nth-child(2)')?.innerHTML).toEqual('banana');
+    expect($('li:nth-child(3)')?.getAttribute('id')).toEqual('3');
+  });
 });
