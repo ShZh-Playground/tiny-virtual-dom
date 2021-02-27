deb34e964e7c8c652371dc4c8f79498345a2215e
Zhihao Shen <zhihaoshen7@qq.com>
2021-02-27 19:52:51 +0800

    🌈 refactor(core): remove useless type assertion.

--- src/vdom.ts
+++ src/vdom.ts
@@ -10,6 +10,32 @@ class El {
     this.attribute = attribute;
     this.children = children;
   }
+
+  render(): HTMLElement {
+    const el = document.createElement(this.tagName);
+
+    if (this.attribute) {
+      for (const key in this.attribute) {
+        const value = this.attribute[key];
+        el.setAttribute(key, value);
+      }
+    }
+
+    if (this.children) {
+      if (typeof this.children === 'string') {
+        const text = document.createTextNode(this.children);
+        el.appendChild(text);
+      } else if (Array.isArray(this.children)) {
+        for (const child of this.children) {
+          el.appendChild(child.render());
+        }
+      } else {
+        el.appendChild(((this.children as unknown) as El).render());
+      }
+    }
+
+    return el;
+  }
 }
 
 export default El;
