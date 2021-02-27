93d79d4e26e79f43ee74b1f19d048dc2ba1986a9
Zhihao Shen <zhihaoshen7@qq.com>
2021-02-27 20:04:55 +0800

    🔨 feat: support DOM manipulation.

--- src/vdom.ts
+++ src/vdom.ts
@@ -2,10 +2,15 @@ type ChildrenType = El | El[] | string | null;
 
 class El {
   tagName: string;
-  attribute: object;
+  attribute: any;
   children: ChildrenType;
 
-  constructor(tagName: string, attribute: object, children: ChildrenType) {
+  constructor(
+    tagName: string,
+    // the attribute field can include any type of attributes
+    attribute: any = null,
+    children: ChildrenType = null
+  ) {
     this.tagName = tagName;
     this.attribute = attribute;
     this.children = children;
@@ -26,7 +31,7 @@ class El {
         const text = document.createTextNode(this.children);
         el.appendChild(text);
       } else if (Array.isArray(this.children)) {
-        for (const child of this.children) {
+        for (const child of this.children as El[]) {
           el.appendChild(child.render());
         }
       } else {
